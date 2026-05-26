a4b8b6842f4c580c86e4bcde1328975dbefb0839
casi <carstem@spraener.de>
2026-05-26 09:18:02 +0200

    Azure-specialized OpenAI chat model supplier

--- prjxp-common/src/main/java/de/spraener/prjxp/common/chat/AzureOpenAPISupplier.java
+++ prjxp-common/src/main/java/de/spraener/prjxp/common/chat/AzureOpenAPISupplier.java
@@ -1,16 +1,25 @@
 package de.spraener.prjxp.common.chat;
 
 import de.spraener.prjxp.common.config.PrjXPChatModelReference;
+import de.spraener.prjxp.common.errorlog.PxLogService;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 /**
  * Supplier for OpenAI-compatible chat models.
  * <p>
@@ -19,6 +28,7 @@ import java.util.Map;
  * </p>
  */
 public class AzureOpenAPISupplier implements ChatModelSupplier {
+    private final PxLogService logService;
     /**
      * Checks if this supplier can provide a chat model for the given reference.
      *
@@ -43,6 +53,9 @@ public class AzureOpenAPISupplier implements ChatModelSupplier {
      */
     @Override
     public ChatModel provide(PrjXPChatModelReference cmRef) {
+        if( cmRef.getArgs().containsKey("trust-all-certs") && cmRef.getArgs().get("trust-all-certs").equals("true") ) {
+            enableInsecureSSL();
+        }
         String apiKey = readArg(cmRef,"api-key");
         return OpenAiChatModel.builder()
                 .baseUrl(cmRef.getProviderUrl()+"/"+cmRef.getModelName()+"/")
@@ -51,6 +64,32 @@ public class AzureOpenAPISupplier implements ChatModelSupplier {
                 .customQueryParams(Map.of("api-version", cmRef.getArgs().get("api-version")))
                 .timeout(Duration.of(15, ChronoUnit.MINUTES))
                 .build();
+    }
+
+    // TODO: Move this to a more global application initializer
+    private void enableInsecureSSL() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            SSLContext.setDefault(sslContext);
+        } catch( NoSuchAlgorithmException | KeyManagementException e) {
+            logService.error(e,"Failed to enable insecure SSL for Azure OpenAI API: %s", e.getMessage());
+        }
     }
 
     private String readArg(PrjXPChatModelReference cmRef, String key) {
