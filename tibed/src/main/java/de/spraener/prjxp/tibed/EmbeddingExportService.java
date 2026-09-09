dd2641d46630734af14365e8d60e041774699a70
carstenSpraener <37332621+carstenSpraener@users.noreply.github.com>
2026-09-09 13:13:57 +0200

    Missing docker configuration

--- tibed/src/main/java/de/spraener/prjxp/tibed/EmbeddingExportService.java
+++ tibed/src/main/java/de/spraener/prjxp/tibed/EmbeddingExportService.java
@@ -71,7 +71,7 @@ public class EmbeddingExportService {
                 }
             }
         } catch (Exception e) {
-            logService.error(e, "Error during embedding export: "+e.getMessage());
+            logService.error(e, "Error during embedding export: "+e.getMessage(), e);
         }
     }
 
